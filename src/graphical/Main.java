a140ee0e5cd298a1ceebee21e32bb7352ca100bd
xuchaoa <xuchaoao@outlook.com>
2018-07-02 19:30:00 +0800

    7.2 17:22

---
+++ src/graphical/Main.java
@@ -0,0 +1,11 @@
+package graphical;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String args[]){
+        login graphical_login = new login();
+        graphical_login.setBounds(500,200,800,800);
+        graphical_login.setTitle("please login");
+    }
+}
